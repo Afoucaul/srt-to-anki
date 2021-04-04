bdac46b1828858a5bbdfcfeb2bb2f75a25932aa7
Armand <armand@cardiologs.com>
2021-04-04 14:49:00 +0200

    Make project

---
+++ srt_parser.py
@@ -0,0 +1,140 @@
+from typing import Any, Dict, List, Tuple
+
+import argparse
+import dataclasses as dc
+import logging
+from multiprocessing import dummy as mp_dummy
+from pathlib import Path
+
+import genanki
+import nagisa
+import regex as re
+import requests
+
+
+LOGGER = logging.getLogger("srt_parser")
+LOGGER.setLevel(logging.INFO)
+
+
+@dc.dataclass
+class Card:
+    front: str
+    back: str
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, Card)
+            and self.front == other.front
+            and self.back == other.back
+        )
+
+    def __hash__(self) -> int:
+        return hash(self.front)
+
+
+def make_anki_deck_from_srt_file(srt_file: Path, *, name: str, output_dir: Path) -> Path:
+    srt_file = srt_file.absolute()
+    deck = genanki.Deck(1, name)
+    model = genanki.Model(
+        1,
+        'Simple Model',
+        fields=[
+            {'name': 'Question'},
+            {'name': 'Answer'},
+        ],
+        templates=[{
+            'name': 'Card 1',
+            'qfmt': '{{Question}}',
+            'afmt': '{{Answer}}',
+        }]
+    )
+
+    cards = make_cards_from_srt_file(srt_file)
+    for card in cards:
+        note = genanki.Note(
+            model=model,
+            fields=[card.front, card.back]
+        )
+        deck.add_note(note)
+
+    output_path = (output_dir / f"{name}.apkg").absolute()
+    genanki.Package(deck).write_to_file(output_path)
+
+    return output_path
+
+
+def make_cards_from_srt_file(srt_file: Path) -> List[Card]:
+    words = get_words_from_srt_file(srt_file)
+    with mp_dummy.Pool(20) as pool:
+        cards = pool.map(make_card_from_word, words)
+
+    return cards
+
+
+def make_card_from_word(word: str) -> Card:
+    LOGGER.info("Making card for word %s", word)
+    jisho_data = search_word_on_jisho(word)
+
+    front = jisho_data["japanese"][0]["word"]
+    back = "【{kana}】: {senses}".format(
+        kana=jisho_data["japanese"][0]["reading"],
+        senses=", ".join(
+            sense["english_definitions"][0] for sense in jisho_data["senses"]
+        )
+    )
+
+    return Card(front, back)
+
+
+def search_word_on_jisho(word: str) -> Dict[Any, Any]:
+    return requests.get(
+        "https://jisho.org/api/v1/search/words",
+        params={"keyword": word}
+    ).json()["data"][0]
+
+
+def get_words_from_srt_file(srt_path: Path) -> List[str]:
+    raw_text = "\n".join(parse_srt_file(srt_path))
+    segments = [
+        segment
+        for segment in nagisa.wakati(raw_text)
+        if re.match(r"^\p{Han}[\p{Han}\p{Hiragana}\p{Katakana}]*$", segment)
+    ]
+    return segments
+
+
+def parse_srt_file(srt_path: Path) -> List[str]:
+    with open(srt_path, "r") as fd:
+        text = fd.read()
+
+    rows = []
+    blocks = text.split("\n\n")
+    for block in blocks:
+        if re.match(r"^\s*$", block):
+            continue
+
+        block_rows = [row for row in block.split("\n") if not re.match(r"^\s*$", row)]
+        rows.extend(block_rows[2:])
+
+    return rows
+
+
+def main(srt_file: Path, *, output_name: str, output_dir: Path) -> None:
+    logging.basicConfig(level=logging.INFO)
+    make_anki_deck_from_srt_file(srt_file, name=output_name, output_dir=output_dir)
+
+
+def make_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--name", required=True, type=str)
+    parser.add_argument("--output-dir", type=Path, default=Path("."))
+    parser.add_argument("srt", type=Path)
+
+    return parser
+
+
+if __name__ == "__main__":
+    argparser = make_argparser()
+    args = argparser.parse_args()
+    main(srt_file=args.srt, output_name=args.name, output_dir=args.output_dir)
---
+++ tests.py
@@ -0,0 +1,87 @@
+import logging
+from pathlib import Path
+import tempfile
+import unittest
+import zipfile
+
+import anki
+import bs4 as bs
+
+from srt_parser import Card
+from srt_parser import get_words_from_srt_file
+from srt_parser import make_anki_deck_from_srt_file
+from srt_parser import make_card_from_word
+from srt_parser import make_cards_from_srt_file
+from srt_parser import parse_srt_file
+
+
+class TestSrtParser(unittest.TestCase):
+    TEST_SRT_PATH = Path("test.srt").absolute()
+    EXPECTED_CARDS = [
+        Card('七', '【しち】: seven, hepta-'),
+        Card('俺', '【おれ】: I'),
+        Card('傷つける', "【きずつける】: to wound, to hurt someone's feelings (pride, etc.), to damage"),
+        Card('誰', '【だれ】: who'),
+        Card('諦める', '【あきらめる】: to give up'),
+        Card('大罪', '【だいざい】: serious crime, Mortal sin'),
+    ]
+
+    def test_srt_parsing(self):
+        rows = parse_srt_file(self.TEST_SRT_PATH)
+        self.assertEqual(len(rows), 4)
+
+    def test_get_words_from_srt(self):
+        expected_words = ["誰", "傷つけ",  "諦", "俺",  "七", "大罪"]
+        actual_words = get_words_from_srt_file(self.TEST_SRT_PATH)
+
+        for word in expected_words:
+            self.assertIn(word, actual_words)
+
+    def test_make_card_from_word(self):
+        word = "誰"
+        card = make_card_from_word(word)
+        self.assertEqual(card.front, word)
+        self.assertEqual(card.back, "【だれ】: who")
+
+    def test_make_cards_from_srt_file(self):
+        actual_cards = make_cards_from_srt_file(self.TEST_SRT_PATH)
+
+        self.assertEqual(set(self.EXPECTED_CARDS), set(actual_cards))
+
+    def test_make_anki_deck_from_srt_file(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            apkg_path = make_anki_deck_from_srt_file(
+                self.TEST_SRT_PATH,
+                name="test",
+                output_dir=Path(tmpdir)
+            )
+            self.assertTrue(apkg_path.is_file())
+
+            with zipfile.ZipFile(apkg_path) as zip:
+                zip.extractall(tmpdir)
+
+            anki2_path = Path(tmpdir) / "collection.anki2"
+            collection = anki.Collection(str(anki2_path))
+            anki_cards = [
+                collection.getCard(card_id)
+                for card_id in collection.findCards("")
+            ]
+
+            self.assertEqual(len(anki_cards), 6)
+
+            anki_cards_as_text = [get_anki_card_as_text(card) for card in anki_cards]
+            self.assertEqual(
+                {f"{card.front}{card.back}" for card in self.EXPECTED_CARDS},
+                set(anki_cards_as_text)
+            )
+
+
+def get_anki_card_as_text(card: anki.cards.Card) -> str:
+    front = html_to_raw_text(card.question())
+    back = html_to_raw_text(card.answer())
+    return f"{front}{back}"
+
+
+def html_to_raw_text(html: str) -> str:
+    soup = bs.BeautifulSoup(html, features="lxml")
+    return soup.text
